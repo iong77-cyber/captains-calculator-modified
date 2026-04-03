db06c02375762d5b96741338d99a37f2bcef02b3
David Melo <davidmelo@desmdesigns.com>
2026-04-02 20:37:17 -0400

    Update package dependencies and refactor version retrieval in loadSettings action

--- src/state/app/actions/loadSettings.ts
+++ src/state/app/actions/loadSettings.ts
@@ -1,10 +1,10 @@
 import { Action } from "state/_types";
-import { version } from '../../../../package.json'
+import packageJson from '../../../../package.json'
 
 export const loadSettings: Action = async ({state,effects}) => {
     let settings = effects.loadLocalStorageSettings()
     if (settings) {
         state.settings = settings
     }
-    state.version = version
+    state.version = packageJson.version
 }
